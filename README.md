0378d7169b91fb34d6f7479c1995fa3cf79c0dc8
Epalma98 <elijahpalma98@yahoo.com>
2023-02-01 20:09:31 -0800

    Finished README

--- README.md
+++ README.md
@@ -1,1 +1,45 @@
-# md6-challenge
+# Weather Dashboard
+This is a webpage that shows the current weather of a user defined city along with the 5-day weather forecast.
+
+## Table of Contents (Optional)
+
+- [Description](#description)
+- [Installation](#installation)
+- [Usage](#usage)
+- [Screenshots](#screenshots) 
+- [Features](#features)
+- [Websites](#websites)
+- [Credit](#credit)
+
+## Description
+I was tasked with creating a webpage that shows different weather conditions based off of a user's input in the search field. The inputted city's temperature, wind speed, and humidity are displayed after entering a city.
+
+I was supposed to add a 5-day forecast listing the same values, but I havent finished that part yet.
+
+## Installation
+1. Download or clone repository
+2. Open the main page (index.html) on your browser to view webpage
+3. Use a text editor to view all coding.
+   
+
+## Usage
+Once the webpage is loaded up the user will need to type in a city name into the search bar and click "search". After doing so, the weather values will list on the page. 
+
+
+## Screenshots
+![Screenshot](/Images/Screenshot%202023-02-01%20at%207.55.52%20PM.png) "Weather Dashboard on start-up")
+![Screenshot](/Images/Screenshot%202023-02-01%20at%207.56.27%20PM.png) "Weather Dashboard with weather values")
+
+## Features
+HTML
+CSS
+JavaScript
+DayJs
+
+
+## Websites
+Github Repo: https://github.com/Epalma98/md5-challenge 
+Deployed website: https://epalma98.github.io/md5-challenge/
+
+## Credit
+Github: Epalma98
--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 // var queryUrl = 'https://api.openweathermap.org/data/2.5/weather?q=Denver&appid=15458b1cfd9511f807c9fce0d75750fe'
 var apiKey = "15458b1cfd9511f807c9fce0d75750fe";
 
+const btnInsert = document.getElementById("submitBtn");
+const cityName = document.getElementById("submitBtn");
+
 let weather = {
     apiKey: "15458b1cfd9511f807c9fce0d75750fe",
     fetchWeather: function (city) {
@@ -15,9 +18,9 @@ let weather = {
     },
     displayWeather: function(data) {
         const { name } = data;
-        const { icon, description } = data.weather[0];
-        const { temp, humidity } = data.main;
-        const { speed } = data.wind;
+        let { icon, description } = data.weather[0];
+        let { temp, humidity } = data.main;
+        let { speed } = data.wind;
 
         document.querySelector(".cityName").innerText = "Current weather in " + name;
         document.querySelector(".icon").src =
@@ -25,28 +28,39 @@ let weather = {
         document.querySelector(".temp").innerText = "Temp: " + temp + "°F";
         document.querySelector(".wind").innerText = "Wind speed: " + speed + "MPH";
         document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%";
-        document.body.style,backgroundImage = "url('https://source.unsplash.com/1600x900/?" + name + "')"
+
     },
-    fetchForecast: function (city) {
-        fetch(
-            "https://api.openweathermap.org/geo/1.0/direct?q="
-            + city 
-            + "&limit=1&appid="
-            + this.apiKey
-        )
-            .then((response) => response.json())
-            .then((data) => console.log(data));
-    },  
-        
 
     search: function () {
         this.fetchWeather(document.querySelector(".search-bar").value);
     }
 };
 
+let forecast = {
+    apiKey: "15458b1cfd9511f807c9fce0d75750fe",
+    fetchForecast: function(city) {
+        fetch(
+            "https://api.openweathermap.org/data/2.5/forecast?q="
+            + city 
+            + "&units=imperial&appid="
+            + this.apiKey
+        )
+            .then((response) => response.json())
+            .then((data) => this.displayForecast(data));
+    },
+    displayForecast: function(data) {
+        let { forecast } = data.list[1].main.temp
+
+        document.querySelector(".forecast-temp1").innerText = "Temp: " + forecast;
+    },
+    search: function () {
+        this.fetchForecast(document.querySelector(".search-bar").value);
+    }
+};
+
 document.querySelector(".col-4 button").addEventListener("click", function() {
     weather.search();
-})
-
-
-
+});
+document.querySelector(".col-4 button").addEventListener("click", function() {
+    forecast.search();
+});
